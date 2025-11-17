6562e002b63c27ab6b70bcf64e6093ccbf144991
abdirahmanmahamoud <abdirahmanmahamoud14@gmail.com>
2025-11-17 19:58:33 +0300

    add desing docs page

--- app/docs/[[...slug]]/page.tsx
+++ app/docs/[[...slug]]/page.tsx
@@ -11,6 +11,8 @@ import {
   IconArrowUpRight,
 } from "@tabler/icons-react";
 import { Badge } from "@/components/ui/badge";
+import mdxComponents from "@/components/mdxComponents";
+import DocsTableOfContents from "@/components/DocsTableOfContents";
 
 const DocsPage = async (props: { params: Promise<{ slug: string[] }> }) => {
   const params = await props.params;
@@ -37,95 +39,27 @@ const DocsPage = async (props: { params: Promise<{ slug: string[] }> }) => {
     .parse(attributes);
 
   return (
-    <div className="flex items-stretch text-[1.05rem] sm:text-[15px] xl:w-full">
-      <div className="flex min-w-0 flex-1 flex-col">
-        <div className="h-(--top-spacing) shrink-0" />
-        <div className="mx-auto flex w-full max-w-2xl min-w-0 flex-1 flex-col gap-8 px-4 py-6 text-neutral-800 md:px-0 lg:py-8 dark:text-neutral-300">
-          <div className="flex flex-col gap-2">
-            <div className="flex flex-col gap-2">
-              <div className="flex items-start justify-between">
-                <h1 className="scroll-m-20 text-4xl font-semibold tracking-tight sm:text-3xl xl:text-4xl">
-                  {doc.title}
-                </h1>
-                <div className="docs-nav bg-background/80 border-border/50 fixed inset-x-0 bottom-0 isolate z-50 flex items-center gap-2 border-t px-6 py-4 backdrop-blur-sm sm:static sm:z-0 sm:border-t-0 sm:bg-transparent sm:px-0 sm:pt-1.5 sm:backdrop-blur-none">
-                  {/* <DocsCopyPage page={raw} url={absoluteUrl(page.url)} /> */}
-                  {neighbours.previous && (
-                    <Button
-                      variant="secondary"
-                      size="icon"
-                      className="extend-touch-target ml-auto size-8 shadow-none md:size-7"
-                      asChild
-                    >
-                      <Link href={neighbours.previous.url}>
-                        <IconArrowLeft />
-                        <span className="sr-only">Previous</span>
-                      </Link>
-                    </Button>
-                  )}
-                  {neighbours.next && (
-                    <Button
-                      variant="secondary"
-                      size="icon"
-                      className="extend-touch-target size-8 shadow-none md:size-7"
-                      asChild
-                    >
-                      <Link href={neighbours.next.url}>
-                        <span className="sr-only">Next</span>
-                        <IconArrowRight />
-                      </Link>
-                    </Button>
-                  )}
-                </div>
-              </div>
-              {doc.description && (
-                <p className="text-muted-foreground text-[1.05rem] text-balance sm:text-base">
-                  {doc.description}
-                </p>
-              )}
-            </div>
-            {links ? (
-              <div className="flex items-center gap-2 pt-4">
-                {links?.doc && (
-                  <Badge asChild variant="secondary" className="rounded-full">
-                    <a href={links.doc} target="_blank" rel="noreferrer">
-                      Docs <IconArrowUpRight />
-                    </a>
-                  </Badge>
-                )}
-                {links?.api && (
-                  <Badge asChild variant="secondary" className="rounded-full">
-                    <a href={links.api} target="_blank" rel="noreferrer">
-                      API Reference <IconArrowUpRight />
-                    </a>
-                  </Badge>
-                )}
-              </div>
-            ) : null}
-          </div>
-          <div className="w-full flex-1 *:data-[slot=alert]:first:mt-0">
-            {/* <MDX components={mdxComponents} /> */}
-          </div>
+    <div className="px-[4%] mt-10 pb-5 flex flex-col lg:flex-row">
+      <div className="w-full lg:w-[75%]">
+        <h1 className="text-3xl font-bold">{doc.title}</h1>
+        {doc.description && (
+          <p className="text-fd-muted-foreground">{doc.description}</p>
+        )}
+        <div className="mt-5">
+          <MDX components={mdxComponents} />
         </div>
-        <div className="mx-auto hidden h-16 w-full max-w-2xl items-center gap-2 px-4 sm:flex md:px-0">
-          {neighbours.previous && (
-            <Button
-              variant="secondary"
-              size="sm"
-              asChild
-              className="shadow-none"
-            >
+        <div className="py-6 flex justify-between items-center">
+          {neighbours.previous ? (
+            <Button variant="secondary" size="sm" asChild>
               <Link href={neighbours.previous.url}>
                 <IconArrowLeft /> {neighbours.previous.name}
               </Link>
             </Button>
+          ) : (
+            <div></div>
           )}
           {neighbours.next && (
-            <Button
-              variant="secondary"
-              size="sm"
-              className="ml-auto shadow-none"
-              asChild
-            >
+            <Button variant="secondary" size="sm" asChild>
               <Link href={neighbours.next.url}>
                 {neighbours.next.name} <IconArrowRight />
               </Link>
@@ -133,17 +67,13 @@ const DocsPage = async (props: { params: Promise<{ slug: string[] }> }) => {
           )}
         </div>
       </div>
-      <div className="sticky top-[calc(var(--header-height)+1px)] z-30 ml-auto hidden h-[calc(100svh-var(--footer-height)+2rem)] w-72 flex-col gap-4 overflow-hidden overscroll-none pb-8 xl:flex">
-        <div className="h-(--top-spacing) shrink-0" />
+      <div className="w-full lg:w-[25%] lg:pl-3 pt-5 lg:pt-0">
         {doc.toc?.length ? (
           <div className="no-scrollbar overflow-y-auto px-8">
-            {/* <DocsTableOfContents toc={doc.toc} /> */}
+            <DocsTableOfContents toc={doc.toc} />
             <div className="h-12" />
           </div>
         ) : null}
-        <div className="flex flex-1 flex-col gap-12 px-6">
-          {/* <OpenInV0Cta /> */}
-        </div>
       </div>
     </div>
   );
