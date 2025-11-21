7309137e8c904d02bd3ff15ff921297e91bd2a2f
abdirahmanmahamoud <abdirahmanmahamoud14@gmail.com>
2025-11-21 18:44:22 +0300

    update release configuration and package versions for semantic release integration

--- package/authverse/release.config.cjs
+++ package/authverse/release.config.cjs
@@ -3,11 +3,24 @@ module.exports = {
     "main",
     {
       name: "beta",
-      prerelease: true,
+      prerelease: "beta",
     },
     {
       name: "canary",
-      prerelease: true,
+      prerelease: "canary",
     },
+  ],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: true,
+        tarballDir: "dist",
+      },
+    ],
+    "@semantic-release/github",
   ],
 };
