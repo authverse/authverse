8c35f22b7aa8d6ea27fe63394e592f8e63106d7b
abdirahmanmahamoud <abdirahmanmahamoud14@gmail.com>
2025-12-26 16:58:59 +0300

    fix: add Drizzle Tanstack

--- package/template/TanstackState/lib/auth-drizzle.ts
+++ package/template/TanstackState/lib/auth-drizzle.ts
@@ -1,11 +1,13 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "../../db/drizzle";
+import { schema } from "../../db/schema";
 import { tanstackStartCookies } from "better-auth/tanstack-start";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
+    schema,
   }),
   emailAndPassword: {
     enabled: true,
