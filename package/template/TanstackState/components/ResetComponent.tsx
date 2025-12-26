6c1442e9c381e7c86a4fe36e207660790e5cf350
abdirahmanmahamoud <abdirahmanmahamoud14@gmail.com>
2025-12-26 18:47:49 +0300

    feat: Add forget and reset password functionality with CLI integration for TanstackState

---
+++ package/template/TanstackState/components/ResetComponent.tsx
@@ -0,0 +1,163 @@
+import { useForm } from "@tanstack/react-form";
+import { toast } from "sonner";
+import * as z from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { authClient } from "@/lib/auth-client";
+import { redirect, useSearch } from "@tanstack/react-router";
+
+const formSchema = z
+  .object({
+    password: z.string().min(8, {
+      message: "Password must be at least 8 characters",
+    }),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
+
+const ResetComponent = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const { token } = useSearch({ from: "/auth/reset-password" });
+
+  const form = useForm({
+    defaultValues: {
+      password: "",
+      confirmPassword: "",
+    },
+    validators: {
+      onSubmit: formSchema,
+    },
+    onSubmit: async ({ value }) => {
+      setIsLoading(true);
+      await authClient.resetPassword(
+        {
+          newPassword: value.password,
+          token,
+        },
+        {
+          onSuccess: () => {
+            toast.success(
+              "Password has been reset successfully. You can now log in with your new password."
+            );
+            redirect({ to: "/" });
+          },
+          onError: (error: any) => {
+            setIsLoading(false);
+            toast.error(error.error.message);
+          },
+        }
+      );
+    },
+  });
+
+  return (
+    <Card className="w-full sm:max-w-md shadow-md">
+      <CardHeader>
+        <CardTitle className="text-2xl leading-4">Reset Password</CardTitle>
+        <CardDescription>Please enter your new password</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form
+          id="form-login"
+          onSubmit={(e) => {
+            e.preventDefault();
+            form.handleSubmit();
+          }}
+        >
+          <FieldGroup>
+            <form.Field
+              name="password"
+              children={(field) => {
+                const isInvalid =
+                  field.state.meta.isTouched && !field.state.meta.isValid;
+                return (
+                  <Field data-invalid={isInvalid}>
+                    <FieldLabel htmlFor={field.name}>Password</FieldLabel>
+                    <Input
+                      id={field.name}
+                      name={field.name}
+                      value={field.state.value}
+                      onBlur={field.handleBlur}
+                      onChange={(e) => field.handleChange(e.target.value)}
+                      aria-invalid={isInvalid}
+                      placeholder="New Password"
+                      type="password"
+                      autoComplete="off"
+                      disabled={isLoading}
+                    />
+                    {isInvalid && (
+                      <FieldError errors={field.state.meta.errors} />
+                    )}
+                  </Field>
+                );
+              }}
+            />
+            <form.Field
+              name="confirmPassword"
+              children={(field) => {
+                const isInvalid =
+                  field.state.meta.isTouched && !field.state.meta.isValid;
+                return (
+                  <Field data-invalid={isInvalid}>
+                    <FieldLabel htmlFor={field.name}>
+                      Confirm Password
+                    </FieldLabel>
+                    <Input
+                      id={field.name}
+                      name={field.name}
+                      value={field.state.value}
+                      onBlur={field.handleBlur}
+                      onChange={(e) => field.handleChange(e.target.value)}
+                      aria-invalid={isInvalid}
+                      placeholder="Confirm Password"
+                      type="password"
+                      autoComplete="off"
+                      disabled={isLoading}
+                    />
+                    {isInvalid && (
+                      <FieldError errors={field.state.meta.errors} />
+                    )}
+                  </Field>
+                );
+              }}
+            />
+          </FieldGroup>
+        </form>
+      </CardContent>
+      <CardFooter>
+        <div className="w-full">
+          <Field orientation="horizontal">
+            <Button
+              type="submit"
+              form="form-login"
+              className="w-full"
+              disabled={isLoading}
+            >
+              {isLoading ? "Resetting..." : "Reset Password"}
+            </Button>
+          </Field>
+        </div>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default ResetComponent;
